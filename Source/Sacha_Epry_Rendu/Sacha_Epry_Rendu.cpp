417f74035af045179accc72509c460d218ada77a
Sacha (Pc) <S.soulslasher@gmail.com>
2024-11-26 17:47:30 +0100

    Main

---
+++ Source/Sacha_Epry_Rendu/Sacha_Epry_Rendu.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Sacha_Epry_Rendu.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sacha_Epry_Rendu, "Sacha_Epry_Rendu" );
---
+++ Source/Sacha_Epry_RenduEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Sacha_Epry_RenduEditorTarget : TargetRules
+{
+	public Sacha_Epry_RenduEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		ExtraModuleNames.Add("Sacha_Epry_Rendu");
+	}
+}
